6f1492a6aa47436d4db699d6cbd53002437164f0
Alejandro Egas <alejoegasg@outlook.com>
2021-05-26 12:13:12 -0500

    Creacion Pagina Principal

--- front-page.php
+++ front-page.php
@@ -1,2 +1,18 @@
 <?php get_header(); ?>
+<main>
+<div class="flex items-center space-x-10">
+<div class="w-1/12">
+<img
+class="rounded-full"
+src="<?php echo get_template_directory_uri() ?>/assets/img/Alejandro.jpg"
+alt="imagen de perfil de shopcenter"
+>
+</div>
+<div class="w-6/12">
+<p> Hola soy Alejandro Egas tengo 22 años soy de Ecuador vivo en la ciudad de Quito, estudio informática </p>
+</div>
+</div>
+</main>
+
+
 <?php get_footer(); ?>
